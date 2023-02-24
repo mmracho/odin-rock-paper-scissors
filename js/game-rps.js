14e1e3aab735eeefbc967c96099a8744dff78888
mmracho <20000699+mmracho@users.noreply.github.com>
2023-02-25 00:53:54 +0800

    Add score counter

--- js/game-rps.js
+++ js/game-rps.js
@@ -1,33 +1,51 @@
 // 1 - ROCK
 // 2 - PAPER
 // 3 - SCISSORS
-
-// 1 - TIE
-// 2 - LOSE
-// 3 - WIN
 let playerScore = 0;
 let computerScore = 0;
+let flavorText = [
+  "Let me analyze my opponent's past moves and determine the most statistically advantageous option.",
+  "Hmm, my opponent seems to favor rock. Maybe I should try paper this time.",
+  "I'm programmed to be unpredictable, so perhaps I'll throw a curveball and choose scissors.",
+  "If I choose the same move twice in a row, will my opponent expect me to switch it up or stay the same?",
+  "I'm confident in my algorithms, but there's always a chance for human error. Let's see how this round plays out.",
+  "Computer is thinking..."
+]
 
 
 const btnReset = document.querySelector('button.btn-reset');
 const buttons = document.querySelectorAll('button.btn-play');
-btnReset.addEventListener('click', resetGame, )
+btnReset.addEventListener('click', resetGame,)
 buttons.forEach(button => {
   button.addEventListener('click', startRound);
 });
 
+function updateScore() {
+  const currentComputerScore = document.querySelector('#computer-score');
+  const currentPlayerScore = document.querySelector('#player-score');
+
+  currentComputerScore.textContent = computerScore;
+  currentPlayerScore.textContent = playerScore;
+}
+
+function randomizeFlavorText() {
+  const currentText = document.querySelector('#computer-flavor-text');
+  const randomTextInd = Math.floor((Math.random() * flavorText.length))
+  currentText.textContent = flavorText[randomTextInd];
+}
+
 function resetGame() {
   playerScore = 0;
   computerScore = 0;
-  // MORE
+  console.log('Game Reset')
 }
 
 function startRound() {
   const playerSelection = this.value;
   const computerSelection = getComputerChoice();
 
+  randomizeFlavorText();
   playRound(playerSelection, computerSelection);
-
 }
 
 function getComputerChoice() {
@@ -66,11 +84,15 @@ function playRound(playerSelection, computerSelection) {
     // console.log('WIN')
     playerScore += 1;
   }
+  
+  updateScore();
+  // Show result
 
   // CHECK STANDING
   console.log(`STANDING: Player=${playerScore} | Computer=${computerScore}`);
   if (checkWinner()) {
-    // TRIGGER RESET STUFF
+    // Show WIN/LOSE
+    // Show reset button
   }
-
+  
 }

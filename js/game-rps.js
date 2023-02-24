155ad654c387e8a7c57a424a9898ea90da1ddf86
mmracho <20000699+mmracho@users.noreply.github.com>
2023-02-24 15:41:12 +0800

    Refactor game logic to use GUI input

--- js/game-rps.js
+++ js/game-rps.js
@@ -5,75 +5,47 @@
 // 1 - TIE
 // 2 - LOSE
 // 3 - WIN
+let playerScore = 0;
+let computerScore = 0;
 
+
+
+const buttons = document.querySelectorAll('button.btn-play');
+buttons.forEach(button => {
+  button.addEventListener('click', getPlayerChoice);
+});
+
+function getPlayerChoice() {
+  const playerSelection = this.value;
+  const computerSelection = getComputerChoice();
+
+  playRound(playerSelection, computerSelection);
+
+}
 
 function getComputerChoice() {
   return choice = Math.floor((Math.random() * 3) + 1);
 }
 
-
-// Choice normalizer - remove when GUI
-function normalizeChoice(choice) {
-  return choice.toLowerCase();
-}
-
-// Choice evaluator - if choice is invalid return 0 (ask again) | if choice is empty exit game
-function evaluateChoice(choice) {
-  switch (choice) {
-    case 'rock':
-      return 1;
-    case 'paper':
-      return 2;
-    case 'scissors':
-      return 3;
-    case 1:
-      return 'rock';
-    case 2:
-      return 'paper';
-    case 3:
-      return 'scissors';
-    case null:
-      return -1;
-    default:
-      return 0;
-  }
-}
-
-// Single round of game - modify when GUI
+// Single round of game
 function playRound(playerSelection, computerSelection) {
-  playerSelection = evaluateChoice(playerSelection);
   if (playerSelection == computerSelection) {
-    return 1; // TIE
+    console.log('TIE')
+    return 1;// TIE
   } else if (playerSelection == 1 && computerSelection == 3) {
+    console.log('WIN')
     return 3; // WIN
   } else if (playerSelection < computerSelection) {
+    console.log('LOSE')
     return 2; // LOSE
   } else {
+    console.log('WIN')
     return 3; // WIN
   }
 }
 
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
   for (i = 0 ; i < 5 ; i++) {
-    const playerSelection = normalizeChoice(prompt('Move: '));
-    const computerSelection = getComputerChoice();
-
-    switch (playRound(playerSelection, computerSelection)) {
-      case 1:
-        console.log('Tie');
-        break;
-      case 2:
-        console.log('Lose');
-        computerScore += 1;
-        break;
-      default:
-        console.log('Win');
-        playerScore += 1;
-    }
-  }
-
   // Winner announce (only on fixed rounds)
   if (playerScore == computerScore) {
     console.log(`It's a tie! Player: ${playerScore} Computer: ${computerScore}`);
@@ -82,6 +54,4 @@ function game() {
   } else {
     console.log(`You win! Player: ${playerScore} Computer: ${computerScore}`);
   }
-}
-
-game();
+}}
